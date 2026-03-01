3ac37468087529a3010e9b4a599ae7550391267e
AnWo <phuan050302@gmail.com>
2026-03-01 20:27:23 +0700

    Add subtract operation

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ function add() {
 
     document.getElementById("result").innerText = "Result: " + result;
 }
+
+function subtract() {
+    const num1 = parseFloat(document.getElementById("num1").value);
+    const num2 = parseFloat(document.getElementById("num2").value);
+
+    const result = num1 - num2;
+
+    document.getElementById("result").innerText = "Result: " + result;
+}
